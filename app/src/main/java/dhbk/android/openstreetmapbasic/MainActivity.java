289ea503ac84e6774e8c052c8508bee0b81f37d7
Phong <p7157568@gmail.com>
2016-03-18 00:05:21 +0700

    feat - make basic map

--- app/src/main/java/dhbk/android/openstreetmapbasic/MainActivity.java
+++ app/src/main/java/dhbk/android/openstreetmapbasic/MainActivity.java
@@ -3,11 +3,17 @@ package dhbk.android.openstreetmapbasic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
     }
 }
